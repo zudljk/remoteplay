d770c4cc6c4a7bc08729d951b93ca3c59b146b7f
Zudljk <zudljk@email.de>
2024-02-08 23:49:04 +0100

    Install from inside script

--- src/remoteplay-runner.py
+++ src/remoteplay-runner.py
@@ -1,8 +1,10 @@
 from remoteplay.__main__ import main as remoteplay
-from sys import argv
+from sys import argv, executable
 
 if len(argv) > 1 and argv[1] == '--create-executable':
     import PyInstaller.__main__
+    import subprocess
+    subprocess.check_call([executable, "-m", "pip", "install", '.'])
     PyInstaller.__main__.run([ __file__, '--onefile', '--name', 'remoteplay', '--exclude-module', 'PyInstaller' ])
 elif __name__ == '__main__':
     remoteplay()
