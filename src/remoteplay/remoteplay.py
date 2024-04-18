11680b02fe4889fc1009690434aa94ffe6b08c2b
zudljk <zudljk@email.de>
2024-04-18 22:22:41 +0200

    Add QT GUI

---
+++ src/remoteplay/remoteplay.py
@@ -0,0 +1,233 @@
+from os import args
+
+PAPERSPACE_API=https://api.paperspace.com/v1
+MACHINE_NAME=args[0]
+API_KEY=args[1]
+MACHINE_ID=None
+SSH_PROCESS=None
+PUBLIC_IP=None
+HOST=None
+
+# Function to retrieve the public IP of the Paperspace machine
+def get_machine(machine):
+    m = next((x for x in request_get()["items"] if x["id"] == machine or x["name"] == machine))
+    return m["id"], m["publicIp"]
+
+
+def request_get(path):
+    response = requests.get(f"{PAPERSPACE_API}/machines/{path}", headers={
+        "accept": "application/json", 
+        "authorization": f"Bearer {API_KEY}"
+    })
+    return loads(response.text)
+
+
+def request_patch():
+    response = requests.patch(f"{PAPERSPACE_API}/machines/{path}", headers={
+        "accept": "application/json", 
+        "authorization": f"Bearer {API_KEY}"
+    })
+    return loads(response.text)
+
+
+
+def change_machine_status(machine_id, command, target_state, status_callback):
+    state = self.check_state(machine_id)
+    status_callback(state)
+
+    if state != target_state:
+        request_patch(f"{machine_id}/{command}"
+        wait_for_state(machine_id, target_state, status_callback)
+
+
+def wait_for_state(machine_id, target_state):
+    state = check_state(machine_id)
+    while state != target_state:
+        sleep(5)
+        state = check_state(machine_id)
+        status_callback(state)
+
+
+# Function to stop the Paperspace machine
+def stop_machine(machine_id, status_callback):
+    tunnel("close")
+    status_callback("shutdown requested")
+    change_machine_status(machine_id, "stop", "off", status_callback)
+
+
+def tunnel(action, hostname):
+    if action == "open":
+        SSH_PROCESS = subprocess.Popen(["ssh", "-N", "-R", "7575:localhost:7575", "-o", "StrictHostKeyChecking=no", hostname])
+    elif action == "close":
+        SSH_PROCESS.terminate()
+    elif action == "check":
+        if not SSH_PROCESS:
+            return "closed"
+        returncode = SSH_PROCESS.poll()
+        if returncode:
+            return "closed"
+        else
+            return "open"
+
+
+def start_usbserver(status_callback):
+    if check_process("vhusb") != "active":
+        subprocess.Popen("/Applications/VirtualHereServerUniversal.app/Contents/MacOS/vhusbdosx")
+    
+
+def determine_host_name(*args):
+    for id in args:
+        process = subprocess.Popen(["ssh", "-G", id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        for line in process.stdout:
+            l = line.strip()
+            if l.startswith('HostName'):
+                return l.split()[1]
+    return None
+
+
+def check_process(string):
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] == process_name:
+            return "active"
+    return "inactive"
+
+
+# Function to check the state of the Paperspace machine
+def check_state(machine_id):
+    return request_get(machine_id)["state"] if machine_id else "unknown"
+
+
+class ChangeMachineStatus(QObject):
+    
+    finished = pyqtSignal()
+    status = pyqtSignal(str)
+    
+    def __init__(self, machine_id):
+        super().__init__()
+        self.machine_id = machine_id
+        self.action = "start"
+        self.target_state = "ready"
+
+
+    def stop(self):
+        self.action = "stop"
+        self.target_state = "off"
+
+
+    def run(self):
+        def callback(state):
+            self.status.emit(state)
+        change_machine_status(self.machine_id, self.action, self.target_state, callback)    
+        self.finished.emit()
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        self.machine_name = MACHINE_NAME
+        self.machine_id, self.public_ip = get_machine(self.machine_name)
+        self.hostname = determine_host_name(self.machine_name, self.machine_id, self.public_ip)
+        self.machine_state = check_state(self.machine_id)
+        
+        self.setWindowTitle('Remote machine')
+        self.layout = QVBoxLayout()
+        
+        self.current_state = {
+            "Machine ID": (lambda: self.machine_id),
+            "Machine Name": (lambda: self.machine_name),
+            "Hostname": (lambda: self.hostname), 
+            "Public IP": (lambda: self.public_ip), 
+            "USB Server": (lambda: check_process("vhusb")),
+            "Machine state": (lambda: self.machine_state), 
+            "SSH Tunnel": (lambda: tunnel("check"))
+        }
+
+        self.label_widgets = []
+        self.text_widgets = []
+
+        for label in self.current_state.keys():
+            hbox = QHBoxLayout()
+            label_widget = QLabel(label)
+            text_widget = QLineEdit()
+            text_widget.setReadOnly(True)
+            hbox.addWidget(label_widget)
+            hbox.addWidget(text_widget)
+            self.label_widgets.append(label_widget)
+            self.text_widgets.append(text_widget)
+            self.layout.addLayout(hbox)
+
+        self.button = QPushButton("Start remote")
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+        self.set_up_button()
+        
+    
+    def set_up_button(self):
+        if self.machine_state == "ready":
+            self.button.setText("Stop remote")
+            self.button.clicked.connect((lambda: self.start_stop_machine("stop")))
+            self.button.setEnabled(True)
+        elif self.machine_state == "off":
+            self.button.setText("Start remote")
+            self.button.clicked.connect((lambda: self.start_stop_machine("start")))
+            self.button.setEnabled(True)
+        else:
+            self.button.setText("Please wait")
+            self.button.setEnabled(False)
+        
+
+    def start_stop_machine(self, action):
+        self.stop_updating()
+        self.button.setText("Starting")
+        self.button.setEnabled(false)
+        self.thread = QThread()
+        self.worker = ChangeMachineStatus(self.machine_id)
+        if action == "stop":
+            self.worker.stop()
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.status_change_complete)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.state.connect(self.update_state)
+        
+        
+    def status_change_complete(self):
+        self.current_state = (lambda: check_state(self.machine_id))
+        self.start_updating()
+        
+        
+    def update_state(self, state):
+        self.machine_state = state
+        self.update()
+
+
+    def start_updating(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(5000)  # Update every 10 seconds
+        self.update()
+        
+
+    def update(self):
+        for label in self.current_state:
+            text_widget.setText(current_state.get(label, "unknown"))
+        self.set_up_button()
+            
+
+    def stop_updating(self):
+        self.timer.stop()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
+
--- start-gameserver.sh
+++ start-gameserver.sh
@@ -6,40 +6,89 @@ API_KEY=$2
 
 # Function to retrieve the public IP of the Paperspace machine
 get_machine_id() {
-    echo $(curl -s -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines | jq -r ".items[] | select(.name == \"$1\") | .id")
+    echo $(request GET | jq -r ".items[] | select(.name == \"$1\") | .id")
 }
 
 get_public_ip() {
-    echo $(curl -s -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines/$1 | jq -r '.publicIp')
+    echo $(request GET $1 | jq -r '.publicIp')
 }
 
-# Function to start the Paperspace machine
-start_machine() {
-    curl -X PATCH -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines/$1/start
+request() {
+    curl -s -X $1 -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines/$2
+}
 
-    # Wait until machine is ready
-    state=off
-    while [ "$state" != "ready" ]; do
+# Function to start/stop the Paperspace machine
+# $1: Machine ID
+# $2: Command
+# $3: Expected final status
+change_machine_status() {
+    state=$(check_state $1)
+    print_status $state
+
+    if [ "$state" != "$3" ] ; then
+        request PATCH $1/$2 > /dev/null
+        wait_for_state $1 $3
+    fi
+    echo
+}
+
+# $1: machine ID
+# $2: expected state
+# $3: command
+wait_for_state() {
+    state=$(check_state $1)
+    while [ "$state" != "$2" ]; do
         sleep 5
         state=$(check_state $1)
+        print_status $state
     done
+}
+
+color() {
+    case $1 in
+    off|stopped)
+        echo -e "\033[37m$1\033[39m      "
+        ;;
+    starting|stopping|shutdown*)
+        echo -e "\033[33m$1\033[39m      "
+        ;;
+    ready|active)
+        echo -e "\033[32m$1\033[39m      "
+        ;;
+    failed|inactive|unknown)
+        echo -e "\033[31m$1\033[39m      "
+        ;;        
+    *)
+        echo -e "$1                  "‚
+        ;;
+    esac
 }
 
 # Function to check the state of the Paperspace machine
 check_state() {
-    echo $(curl -s -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines/$1 | jq -r '.state')
+    echo $(request GET $1 | jq -r '.state')
+}
+
+start_machine() {
+    change_machine_status $1 start ready
 }
 
 # Function to stop the Paperspace machine
 stop_machine() {
-    curl -X PATCH -H "Authorization: Bearer $API_KEY" $PAPERSPACE_API/machines/$1/stop
+    tunnel close
+    print_status "shutdown requested"
+    change_machine_status $1 stop off
+}
 
-    # Wait until machine is stopped
-    state=ready
-    while [ "$state" != "off" ]; do
-        sleep 5
-        state=$(check_state $1)
-    done
+tunnel() {
+    case $1 in
+    open)
+        ssh -N -R 7575:localhost:7575 -o StrictHostKeyChecking=no $2 &
+        ;;
+    close)
+        ps -o pid,command | grep -v grep | grep "7575:localhost:7575" | while read pid rest ; do kill $pid ; done
+        ;;
+    esac
 }
 
 determine_host_name() {
@@ -52,29 +101,66 @@ determine_host_name() {
     done
 }
 
-echo "Starting USB server ..."
-/Applications/VirtualHereServerUniversal.app/Contents/MacOS/vhusbdosx || echo "Cannot start USB server; maybe already started?"
+checkprocess() {
+    ps | grep -v grep | grep -q $1 && echo "active" || echo "inactive"
+}
+
+print_status() {
+    tput cup 0 0
+    echo "Machine name:  $MACHINE_NAME"
+    echo "Machine ID:    $MACHINE_ID"
+    echo "Hostname:      $HOST"
+    echo -n "USB server:    "
+    color $(checkprocess vhusb)
+    echo -n "Machine state: "
+    color $1
+    echo -n "SSH Tunnel:    "
+    color $(checkprocess 7575:localhost:7575)
+    echo
+    case $1 in
+    starting)
+        echo "Remote server starting up ..."
+        ;;
+    stopping)
+        echo "Remote server shutting down ..."
+        ;;
+    ready)
+        echo "Press Ctrl-C to stop the machine and close the SSH tunnel."
+        ;;
+    esac
+}
+
 
 # Get the Paperspace ID by name
 MACHINE_ID=$(get_machine_id $MACHINE_NAME)
 
+# Trap Ctrl-C to stop the machine and close the SSH tunnel
+trap 'stop_machine $MACHINE_ID ; exit' INT
+
 # Get the public IP of the machine
 PUBLIC_IP=$(get_public_ip $MACHINE_ID)
-
 HOST=$(determine_host_name $MACHINE_NAME $MACHINE_ID $PUBLIC_IP)
+
+tput clear
+print_status unkown
+
+if [ $(checkprocess vhusb) != "active" ] ; then
+    /Applications/VirtualHereServerUniversal.app/Contents/MacOS/vhusbdosx
+fi
+
+print_status unknown
 
 # Start the machine
 start_machine $MACHINE_ID
 
-# Create SSH tunnel
-echo "Creating SSH tunnel..."
-ssh -N -R 7575:localhost:7575 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $HOST &
+tunnel open $HOST
 
-# Trap Ctrl-C to stop the machine and close the SSH tunnel
-trap 'stop_machine $MACHINE_ID ; exit' INT
+state=ready
 
-# Wait until user presses Ctrl-C
-echo "Press Ctrl-C to stop the machine and close the SSH tunnel..."
-while true; do
-    sleep 1
+while [ "$state" == "ready" ]; do
+    print_status $state
+    sleep 10
+    state=$(check_state $MACHINE_ID)
 done
+wait_for_state $MACHINE_ID off stop
+tunnel close
