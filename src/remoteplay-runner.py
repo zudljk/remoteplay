aa75877a0d61a42f5a23f70cd1cd789dd1cfdb1a
Oliver Meyer <oliver.meyer@nagarro.com>
2024-04-30 22:40:04 +0200

    Create Windows installer package

--- src/remoteplay-runner.py
+++ src/remoteplay-runner.py
@@ -8,10 +8,12 @@ if len(argv) > 1 and argv[1] == '--create-executable':
 
     subprocess.check_call([executable, "-m", "pip", "install", '.'])
     if system() == 'Darwin':  # Linux and macOS
-        PyInstaller.__main__.run([__file__, '--windowed', '--name', 'remoteplay', '--exclude-module', 'PyInstaller'])
+        PyInstaller.__main__.run([__file__, '--windowed', '--name', 'remoteplay', '--icon', 'images/remoteplay.icns',
+                                  '--exclude-module', 'PyInstaller'])
     elif system() == 'Windows':  # Windows
-        PyInstaller.__main__.run([__file__, '--windowed', '--onefile', '--name', 'remoteplay', '--distpath',
-                                  'application', '--exclude-module', 'PyInstaller'])
+        PyInstaller.__main__.run([__file__, '--windowed', '--onefile', '--name', 'remoteplay',
+                                  '--icon', 'images/remoteplay.ico', '--distpath', 'application', '--exclude-module',
+                                  'PyInstaller'])
     else:
         PyInstaller.__main__.run([__file__, '--onefile', '--name', 'remoteplay', '--exclude-module', 'PyInstaller'])
 elif __name__ == '__main__':
