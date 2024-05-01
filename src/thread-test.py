dc84f2847a6e99edc3c58ac2ab5fbdc049917889
Oliver Meyer <oliver.meyer@nagarro.com>
2024-05-01 23:43:15 +0200

    Test for threading bug

--- src/thread-test.py
+++ src/thread-test.py
@@ -1,21 +1,15 @@
 import sys
-
 from PyQt5.QtCore import QObject, QThread
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QWidget
 
-
 class LongRunningThread(QObject):
-
     def __init__(self, name):
         super().__init__()
         self.name = name
-
     def run(self):
         while True:
             print(f"Waiting for {self.name} ...")
             QThread.sleep(5)
-
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -24,7 +18,6 @@ class MainWindow(QMainWindow):
         self.start_thread_button.clicked.connect(self.start_thread)
         self.setCentralWidget(self.central_widget)
         self.start_thread_button.setText("Start Thread")
-
     def start_thread(self):
         self.start_thread_button.setEnabled(False)
         self.thread = QThread()
@@ -33,13 +26,8 @@ class MainWindow(QMainWindow):
         self.thread.started.connect(self.worker.run)
         self.thread.start()
 
-
-def main():
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
