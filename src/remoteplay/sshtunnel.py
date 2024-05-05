0041080a09391a1d04709deba4caacff015c0864
zudljk <zudljk@email.de>
2024-05-05 23:58:24 +0200

    Use CREATE_NO_WINDOW for Windows subprocesses

--- src/remoteplay/sshtunnel.py
+++ src/remoteplay/sshtunnel.py
@@ -1,16 +1,28 @@
 import subprocess
+from platform import system
+
 
 class SshTunnel:
-
     def __init__(self):
-        self.host_provider = (lambda: None)
+        self.host_provider = lambda: None
         self.process = None
 
     def open(self):
         if self.host_provider() is not None and not isinstance(self.process, subprocess.Popen):
-            self.process = subprocess.Popen(["ssh", "-N", "-R", "7575:localhost:7575",
-                                             "-o", "StrictHostKeyChecking=no", self.host_provider()])
-
+            command = [
+                "ssh",
+                "-N",
+                "-R",
+                "7575:localhost:7575",
+                "-o",
+                "StrictHostKeyChecking=no",
+                self.host_provider(),
+            ]
+            if system() == 'Windows':
+                self.process = subprocess.Popen(command, creationflags=subprocess.CREATE_NO_WINDOW)
+            else:
+                self.process = subprocess.Popen(command)
+            
     def close(self):
         if self.process:
             self.process.terminate()
