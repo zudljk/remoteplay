25c87db6cd2dd86ec839cb48aa47d8b737c6252c
zudljk <zudljk@email.de>
2024-05-11 12:06:06 +0200

    8 windows show some info somewhere that the cmd window for the ssh subprocess should not be closed (#13)
    * Use CREATE_NO_WINDOW for Windows subprocesses
    
    * Garbage collect the SSH process after shutdown
    
    ---------
    
    Co-authored-by: Oliver Meyer <oliver.meyer@nagarro.com>

--- src/remoteplay/sshtunnel.py
+++ src/remoteplay/sshtunnel.py
@@ -1,20 +1,33 @@
 import subprocess
+from platform import system
+
 
 
 class SshTunnel:
-
     def __init__(self):
-        self.host_provider = (lambda: None)
+        self.host_provider = lambda: None
         self.process = None
 
     def open(self):
         if self.host_provider() is not None and self.process is None:
-            self.process = subprocess.Popen(["ssh", "-N", "-R", "7575:localhost:7575",
-                                             "-o", "StrictHostKeyChecking=no", self.host_provider()])
+            command = [
+                "ssh",
+                "-N",
+                "-R",
+                "7575:localhost:7575",
+                "-o",
+                "StrictHostKeyChecking=no",
+                self.host_provider(),
+            ]
+            if system() == 'Windows':
+                self.process = subprocess.Popen(command, creationflags=subprocess.CREATE_NO_WINDOW)
+            else:
+                self.process = subprocess.Popen(command)
 
     def close(self):
         if self.process:
             self.process.terminate()
+            self.process = None
 
     def check(self):
         if not self.process:
